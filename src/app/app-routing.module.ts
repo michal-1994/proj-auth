7227ff4b338f0ad38dcefd865d7f0aa537db7a8b
Michal <michalgrzegorczyk04@gmail.com>
2023-08-20 09:05:10 +0200

    feat: add auth components and create routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './auth/components/login/login.component';
+import { SignupComponent } from './auth/components/signup/signup.component';
 import { HomeComponent } from './home/pages/home/home.component';
 
 const routes: Routes = [
     {
         path: '',
         component: HomeComponent
+    },
+    {
+        path: 'login',
+        component: LoginComponent
+    },
+    {
+        path: 'sign-up',
+        component: SignupComponent
     }
 ];
 
--- src/app/core/components/header/header.component.ts
+++ src/app/core/components/header/header.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
     styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent {
-    logout() {}
+    logout() {
+        console.log('logout');
+    }
 }
