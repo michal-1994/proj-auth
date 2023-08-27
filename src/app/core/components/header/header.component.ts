ab62963d33830f88ba16618030849df12da1ec71
Michal <michalgrzegorczyk04@gmail.com>
2023-08-27 12:22:13 +0200

    feat: implementing tailwind styles

--- src/app/core/components/header/header.component.ts
+++ src/app/core/components/header/header.component.ts
@@ -1,12 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 
 @Component({
     selector: 'app-header',
     templateUrl: './header.component.html',
     styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
+    menuVisible!: boolean;
+
+    @HostListener('window:resize', ['$event'])
+    onWindowResize() {
+        this.setInitialMenuVisibility();
+    }
+
+    ngOnInit(): void {
+        this.setInitialMenuVisibility();
+    }
+
     logout() {
         console.log('logout');
+    }
+
+    toggleMenu() {
+        if (window.innerWidth < 768) {
+            this.menuVisible = !this.menuVisible;
+        }
+    }
+
+    setInitialMenuVisibility() {
+        this.menuVisible = window.innerWidth > 767;
     }
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,18 @@
 module.exports = {
     content: ['./src/**/*.{html,ts}'],
     theme: {
-        extend: {}
+        extend: {
+            colors: {
+                primary: '#3452ed',
+                accent: '#191919',
+                warn: '#ffc107',
+                warn100: '#d39e00',
+                light: '#ffffff',
+                light100: '#efefef',
+                dark: '#000000',
+                ground: '#232323'
+            }
+        }
     },
     plugins: []
 };
